bbbb04041ebc2f2d4e0a91cd10118a98a3a0548c
Dan Morse <morsedan@outlook.com>
2019-06-12 10:04:07 -0700

    added Question

---
+++ QandAPractice/Model/Question.swift
@@ -0,0 +1,23 @@
+//
+//  Question.swift
+//  QandAPractice
+//
+//  Created by morse on 6/12/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let asker: String
+    var questionText: String
+    let answerer: String?
+    var answer: String?
+    
+    init(asker: String, questionText: String, answerer: String? = nil, answer: String? = nil) {
+        self.asker = asker
+        self.questionText = questionText
+        self.answerer = answerer
+        self.answer = answer
+    }
+}
---
+++ QandAPractice/Model/QuestionCell.swift
@@ -0,0 +1,10 @@
+//
+//  QuestionCell.swift
+//  QandAPractice
+//
+//  Created by morse on 6/12/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
